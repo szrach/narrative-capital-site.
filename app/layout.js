1785dd45f684a3d4ebc542b8ff66d95386cbe090
szrach <seb.zap@outlook.com>
2025-08-19 21:31:55 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,9 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-background text-foreground">
+      <body>
         <Header />
-        {children}
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
