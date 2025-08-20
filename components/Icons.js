056b619ac80c780529f67ca9cf0bb2ff86bea3ea
szrach <seb.zap@outlook.com>
2025-08-20 21:50:37 +0200

    Create Icons.js

---
+++ components/Icons.js
@@ -0,0 +1,22 @@
+export function ShieldIcon(props){ // Exchange readiness
+  return (
+    <svg viewBox="0 0 24 24" width="20" height="20" fill="none" stroke="currentColor" strokeWidth="1.6" {...props}>
+      <path d="M12 3l7 3v5c0 5-3.5 8.5-7 10-3.5-1.5-7-5-7-10V6l7-3z"/>
+      <path d="M9.5 12.5l1.8 1.8 3.7-3.8"/>
+    </svg>
+  );
+}
+export function SpeakerIcon(props){ // Community & distribution
+  return (
+    <svg viewBox="0 0 24 24" width="20" height="20" fill="none" stroke="currentColor" strokeWidth="1.6" {...props}>
+      <path d="M4 9h4l6-5v16l-6-5H4z"/><path d="M16 10.5a3.5 3.5 0 010 3"/><path d="M18 8a6 6 0 010 8"/>
+    </svg>
+  );
+}
+export function HandshakeIcon(props){ // Partnerships
+  return (
+    <svg viewBox="0 0 24 24" width="20" height="20" fill="none" stroke="currentColor" strokeWidth="1.6" {...props}>
+      <path d="M7 15l3-3 3 3 4-4"/><path d="M3 12l4-4 5 5"/><path d="M17 11l4 4"/>
+    </svg>
+  );
+}
